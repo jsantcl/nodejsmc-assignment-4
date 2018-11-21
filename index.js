2befece65491bb10832149ca8ee07f52fb11be7d
Javier S. A <jsantander@outlook.com>
2018-11-21 02:20:20 -0300

    Stripe and Mailgun API integration, email worker fix

--- index.js
+++ index.js
@@ -6,14 +6,19 @@
 
 //Dependencies
 const server = require("./lib/server");
-
+const workers = require("./lib/workers");
 let app = {};
 
 //init function
 app.init = ()=>{
     //start the server
     server.init();
+
+    //start the workers
+    workers.init();
 };
+
+
 
 //execute function
 app.init();
